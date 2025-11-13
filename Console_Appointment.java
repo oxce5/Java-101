422c1e0bab87cf2404e7862416d5368dc87c2c48
avg.gamer <avg.gamer@proton.me>
2025-11-13 10:58:57 +0800

    create UI method

--- Console_Appointment.java
+++ Console_Appointment.java
@@ -3,8 +3,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Console_Appointment {
+  static Scanner scan = new Scanner(System.in);
+  public static int UI() {
+    System.out.println("APPOINTMENTS");
+    System.out.println("( 1 ) Show Appointment/s");
+    System.out.println("( 2 ) Make Appointment/s");
+    System.out.println("( 3 ) Exit");
+    int choice = scan.nextInt();
+    return choice;
+  }
+
   public static void main(String[] args) {
     List<Appointment> appointments = new ArrayList<>(); 
+    switch(UI()) {
+
+    }
   } 
 }
 
