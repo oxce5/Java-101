2672ffcf9fb6fbd6eddc600b27623c45c3b7bffd
avg.gamer <avg.gamer@proton.me>
2025-11-13 10:51:06 +0800

    initialize Console_Appointment class

--- Console_Appointment.java
+++ Console_Appointment.java
@@ -1,4 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Console_Appointment {
+  public static void main(String[] args) {
+    List<Appointment> appointments = new ArrayList<>(); 
+  } 
 }
+
