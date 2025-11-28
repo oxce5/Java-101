1f9f3b9bf55bfddd8724bebcb75534337b7e5e60
avg.gamer <avg.gamer@proton.me>
2025-11-28 11:25:18 +0800

    add Products.java

--- Products.java
+++ Products.java
@@ -1,31 +1,38 @@
 import java.util.Scanner;
 
 public class Products {
-  public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
-    System.out.print("Enter number of products: ");
-    int quantity = Integer.parseInt(scan.nextLine());
+  static Scanner scan = new Scanner(System.in);
+  static int stock = 0, sum = 0;
 
-    int stock = 0, sum = 0;
-    String productName[] = new String[quantity];
-    int productNumber[] = new int[quantity];
-
+  public static String[] ProductManager(int quantity) {
+    String[] products = new String[quantity];
     for (int i = 0; i < quantity; i++) {
       System.out.print("Enter product " + (i+1) +" name: ");
-      productName[i] = scan.nextLine();
-      System.out.print("Enter product " + (i+1) +" stock: ");
-      productNumber[i] = Integer.parseInt(scan.nextLine());
-      sum += productNumber[i];
-      if ( productNumber[i] > 0) stock++;
+      String productName = scan.nextLine();
+      System.out.print("Enter " + productName +" stock: ");
+      int productQuantity =  Integer.parseInt(scan.nextLine());
+      sum += productQuantity;
+      if ( productQuantity > 0) stock++;
+      products[i] = String.format("%s - %d", productName, productQuantity);
     }
+    return products;
+  };
 
+  public static void FetchProducts(String[] products) {
     System.out.println("\nProducts and Quantities");
-    for (int i = 0; i < quantity; i++) {
-      System.out.println(productName[i] + " - " + productNumber[i]);
+    for (String product : products) {
+      System.out.println(product);
     }
 
     System.out.print("\nTotal: " + sum);
-    System.out.printf("\nIn Stock: %d\nOut of Stock: %d", stock, (quantity - stock));
+    System.out.printf("\nIn Stock: %d\nOut of Stock: %d", stock, (products.length - stock));
+  }
+
+  public static void main(String[] args) {
+    System.out.print("Enter number of products: ");
+    int quantity = Integer.parseInt(scan.nextLine());
+    String[] products = ProductManager(quantity);
+    FetchProducts(products);
     scan.close();
   }
 }
